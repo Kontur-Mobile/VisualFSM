6e7a7de279ae8e600a70b6da3a723ebe0b5531f1
Рылов Василий Дмитриевич <vasilyrylov@skbkontur.ru>
2022-05-23 19:03:29 +0500

    Add tests sample to README

--- sample-rx/src/test/kotlin/ru/kontur/mobile/visualfsm/SampleTests.kt
+++ sample-rx/src/test/kotlin/ru/kontur/mobile/visualfsm/SampleTests.kt
@@ -12,9 +12,9 @@ class SampleTests {
     fun generateDigraph() {
         println(
             VisualFSM.generateDigraph(
-                AuthFSMAction::class,
-                AuthFSMState::class,
-                AuthFSMState.Login::class,
+                baseAction = AuthFSMAction::class,
+                baseState = AuthFSMState::class,
+                initialState = AuthFSMState.Login::class,
             )
         )
         Assertions.assertTrue(true)
@@ -23,14 +23,27 @@ class SampleTests {
     @Test
     fun allStatesReachableTest() {
         val notReachableStates = VisualFSM.getUnreachableStates(
-            AuthFSMAction::class,
-            AuthFSMState::class,
-            AuthFSMState.Login::class,
+            baseAction = AuthFSMAction::class,
+            baseState = AuthFSMState::class,
+            initialState = AuthFSMState.Login::class,
         )
 
         Assertions.assertTrue(
             notReachableStates.isEmpty(),
             "FSM have unreachable states: ${notReachableStates.joinToString(", ")}"
+        )
+    }
+
+    @Test
+    fun oneFinalStateTest() {
+        val finalStates = VisualFSM.getFinalStates(
+            baseAction = AuthFSMAction::class,
+            baseState = AuthFSMState::class,
+        )
+
+        Assertions.assertTrue(
+            finalStates.size == 1 && finalStates.contains(AuthFSMState.UserAuthorized::class),
+            "FSM have not correct final states: ${finalStates.joinToString(", ")}"
         )
     }
 }
