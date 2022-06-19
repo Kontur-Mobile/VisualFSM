e7275546e87b16740beb37d656045af90cf5015b
g0rd1 <gordigev@gmail.com>
2022-06-19 21:30:52 +0500

    not use java class

--- visualfsm-tools/src/main/kotlin/ru/kontur/mobile/visualfsm/tools/GeneratedTransactionFactoryProvider.kt
+++ visualfsm-tools/src/main/kotlin/ru/kontur/mobile/visualfsm/tools/GeneratedTransactionFactoryProvider.kt
@@ -9,8 +9,8 @@ object GeneratedTransactionFactoryProvider {
 
     @Suppress("UNCHECKED_CAST")
     inline fun <reified STATE : State, ACTION : Action<STATE>> provide(): TransitionFactory<STATE, ACTION> {
-        val packageName: String? = STATE::class.java.packageName
-        val implName = "Generated${STATE::class.java.simpleName}TransactionFactory"
+        val packageName = STATE::class.qualifiedName?.split(".")?.dropLast(1)?.joinToString(".")
+        val implName = "Generated${STATE::class.simpleName!!}TransactionFactory"
         val implQualifiedName = if (packageName.isNullOrBlank()) implName else "${packageName}.${implName}"
         val kClass = Class.forName(implQualifiedName).kotlin
         return kClass.primaryConstructor!!.call() as TransitionFactory<STATE, ACTION>
