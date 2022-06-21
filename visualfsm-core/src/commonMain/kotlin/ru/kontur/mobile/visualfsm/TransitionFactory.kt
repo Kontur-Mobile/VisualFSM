29694ee41f5e3d1b0b199d8a817941c701e520fe
g0rd1 <gordigev@gmail.com>
2022-06-21 23:47:10 +0500

    add javadoc

--- visualfsm-core/src/commonMain/kotlin/ru/kontur/mobile/visualfsm/TransitionFactory.kt
+++ visualfsm-core/src/commonMain/kotlin/ru/kontur/mobile/visualfsm/TransitionFactory.kt
@@ -1,5 +1,13 @@
 package ru.kontur.mobile.visualfsm
 
+/** Factory for creating transitions described in action */
 interface TransitionFactory<STATE : State, ACTION : Action<STATE>> {
+
+    /**
+     * Returns a list of transitions described in action
+     *
+     * @param action action for which need a list of transitions
+     * @return a list of transitions described in action
+     */
     fun create(action: ACTION): List<Transition<out STATE, out STATE>>
 }
--- visualfsm-tools/src/main/kotlin/ru/kontur/mobile/visualfsm/tools/GeneratedTransactionFactoryProvider.kt
+++ visualfsm-tools/src/main/kotlin/ru/kontur/mobile/visualfsm/tools/GeneratedTransactionFactoryProvider.kt
@@ -5,12 +5,16 @@ import ru.kontur.mobile.visualfsm.State
 import ru.kontur.mobile.visualfsm.TransitionFactory
 import kotlin.reflect.full.primaryConstructor
 
-
+/** Tool class for getting instances of generated TransactionFactory implementations */
 class GeneratedTransactionFactoryProvider {
 
+    /**
+     * Provide an instance of the generated TransactionFactory for the given typed state and action parameters.
+     * @return an instance of the generated TransactionFactory
+     */
     @Suppress("UNCHECKED_CAST")
     inline fun <reified STATE : State, ACTION : Action<STATE>> provide(): TransitionFactory<STATE, ACTION> {
-        val packageName = STATE::class.qualifiedName?.split(".")?.dropLast(1)?.joinToString(".")
+        val packageName = STATE::class.qualifiedName?.split(".")?.takeIf { it.size > 1 }?.dropLast(1)?.joinToString(".")
         val implName = "Generated${STATE::class.simpleName!!}TransactionFactory"
         val implQualifiedName = if (packageName.isNullOrBlank()) implName else "${packageName}.${implName}"
         val kClass = Class.forName(implQualifiedName).kotlin
