9e7c60574fd94e9ea9b8779457512d8f14ea9565
Evgeny Meltsaykin <mel.ea@skbkontur.ru>
2024-04-09 20:55:24 +0500

    Refactoring gradle files

---
+++ visualfsm-core/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    alias(libs.plugins.multiplatform)
+    id("kotlinx-atomicfu")
+}
+apply(from = "../publish.gradle")
+
+group = rootProject.group
+version = rootProject.version
+
+kotlin {
+    jvm {
+        jvmToolchain(17)
+        withJava()
+        testRuns.named("test") {
+            executionTask.configure {
+                useJUnitPlatform()
+            }
+        }
+    }
+    js { browser { } }
+    iosArm64()
+    iosX64()
+    iosSimulatorArm64()
+    macosX64()
+    linuxX64()
+    mingwX64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+        }
+    }
+}
