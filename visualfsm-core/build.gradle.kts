8469351847c967ff29b3c76ef417d9a658739a9a
Vasily Rylov <vasiliyrylov@yandex.com>
2024-12-22 10:03:54 +0500

    Update dependencies and kmp targets

--- visualfsm-core/build.gradle.kts
+++ visualfsm-core/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.multiplatform)
     alias(libs.plugins.atomicfu)
@@ -9,21 +11,50 @@ version = rootProject.version
 
 kotlin {
     jvmToolchain(17)
-    jvm {
-        withJava()
-        testRuns.named("test") {
-            executionTask.configure {
-                useJUnitPlatform()
-            }
-        }
+    jvm()
+
+    js {
+        browser()
+        nodejs()
     }
-    js { browser { } }
-    iosArm64()
-    iosX64()
-    iosSimulatorArm64()
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        nodejs()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmWasi {
+        nodejs()
+    }
+
+    // According to https://kotlinlang.org/docs/native-target-support.html
+    // Tier 1
     macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    iosArm64()
+
+    // Tier 2
     linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+
+    // Tier 3
     mingwX64()
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+    watchosDeviceArm64()
 
     sourceSets {
         commonMain.dependencies {
